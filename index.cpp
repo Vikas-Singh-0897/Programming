2e28e1c7c1462ed2ec756ac39d6c5c6a25e28d02
Vikas-Singh-0897 <vikasrajput0897@gmail.com>
2025-08-12 22:20:53 +0530

    Day-2

--- index.cpp
+++ index.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+// Bubble Sort
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+// Main Function
 int main()
 {
-    cout << "Hello World" << endl;
+    int arr[] = {1, 3, 5, 2, 1, 4, 5, 6, 7, 8};
+    int size = 10;
+    cout << "Welcome to the programme" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    bubbleSort(arr, size);
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
